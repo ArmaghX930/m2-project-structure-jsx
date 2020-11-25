816d9d4775cba9b560745e779422253ae8431f01
tlapi <tasha.lapitan@gmail.com>
2020-11-25 19:26:17 +0100

    style layout, add logo and icons

--- views/Home.jsx
+++ views/Home.jsx
@@ -6,51 +6,54 @@ function Home(props) {
   return (
     <Layout title="spaceOut" user={props.user ? props.user : ""}>
       <main>
-        <h2>Description that blah blah</h2>
-        <div>
-          <div id="search-container">
-            <form action="/search" method="GET">
-              <label htmlFor="city">City </label>
-              <input list="cities" name="city" />
-              <datalist id="cities">
-                 {props.cities.map((city, i) => {
-                    return (
-                      <option key={i} value={city} />
-                    )
-                 })}
-              </datalist><br/>
-              <label htmlFor="pricePerHour">Maximum Price </label>
-              <input type="Number" name="pricePerHour"/><span>€ per hour</span><br/>
-              <label htmlFor="capacity">Capacity </label>
-              <input type="Number" name="capacity"/><span> people</span><br/>
-              <label htmlFor="availToday">Available Today</label>
-              <input type="checkbox" name="availToday" value="true" /><br/>
-              <button type="submit">Search</button>
-            </form>
+        <section>
+          <h2>Description that blah blah</h2>
+          <div>
+            <div id="search-container">
+              <form action="/search" method="GET">
+                <label htmlFor="city">City </label>
+                <input list="cities" name="city" />
+                <datalist id="cities">
+                  {props.cities.map((city, i) => {
+                      return (
+                        <option key={i} value={city} />
+                      )
+                  })}
+                </datalist><br/>
+                <label htmlFor="pricePerHour">Maximum Price </label>
+                <input type="Number" name="pricePerHour"/><span>€ per hour</span><br/>
+                <label htmlFor="capacity">Capacity </label>
+                <input type="Number" name="capacity"/><span> people</span><br/>
+                <label htmlFor="availToday">Available Today</label>
+                <input type="checkbox" name="availToday" value="true" /><br/>
+                <button type="submit">Search</button>
+              </form>
+            </div>
+            <div id="space-container">
+            {props.spaces.map((spaceObj, i) => {
+              return (
+                <SpaceCard space={spaceObj} key={i}>
+                </SpaceCard>
+              )
+            })}
+            </div>
           </div>
-          <div id="space-container">
-           {props.spaces.map((spaceObj, i) => {
-             return (
-               <SpaceCard space={spaceObj} key={i}>
-               </SpaceCard>
-             )
-           })}
-          </div>
-        </div>
+        </section>
+        <article id="provider-ad">
+          <p>aggressive provider recruitment</p>
+          {props.user 
+          ? <a href="/user"><div id="become-provider-btn">BECOME SPACE PROVIDER</div></a>
+          : <a href="/auth"><div id="become-provider-btn">BECOME SPACE PROVIDER</div></a>
+          }
+        </article>
+        <section>
+          <div className="space-card">SPACE CARD COMPONENT</div>
+          <div className="space-card">SPACE CARD COMPONENT</div>
+          <div className="space-card">SPACE CARD COMPONENT</div>
+          <div className="space-card">SPACE CARD COMPONENT</div>
+        </section>
       </main>
-      <article id="provider-ad">
-        <p>aggressive provider recruitment</p>
-        {props.user 
-        ? <a href="/user"><div id="become-provider-btn">BECOME SPACE PROVIDER</div></a>
-        : <a href="/auth"><div id="become-provider-btn">BECOME SPACE PROVIDER</div></a>
-         }
-      </article>
-      <section>
-        <div className="space-card">SPACE CARD COMPONENT</div>
-        <div className="space-card">SPACE CARD COMPONENT</div>
-        <div className="space-card">SPACE CARD COMPONENT</div>
-        <div className="space-card">SPACE CARD COMPONENT</div>
-      </section>
+      
     </Layout>)
 }
 
--- views/Layout.jsx
+++ views/Layout.jsx
@@ -7,30 +7,34 @@ function Layout(props) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" /> 
         <title> {props.title ? props.title : "spaceOut"} </title>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet"/>
         <link rel="stylesheet" href="/stylesheets/style.css" />
       </head>
       <body>
-        <header>
-          <a href="/"><div>Logo</div></a>
+        <header id="layout-header" class="layout">
+          <a href="/"><div> <img src="/images/logo.png" width="200"/> </div></a>
           <h3>Self explanatory catchphrase</h3>
           {props.user 
-          ? ( <div>
+          ? ( <div id="login-header-div">
                 <a href={`/user`}>
                   <div>
-                    {props.user.username}
+                    <img class="layout-profile-pic" src={props.user.imageUrl}/>
                   </div>
                 </a>
                 <a href="/auth/logout">
-                <div>Logout button</div>
+                <div>
+                  <img src="/images/logout-btn.png" width="25" height="25"/>
+                </div>
                 </a>
               </div>) 
-          : (<a href="/auth/"><div>Absence of userpic</div></a>)}
+          : (<a href="/auth/"><div> <img class="layout-profile-pic" src="/images/profile-image-placeholder.png"/> </div></a>)}
         </header>
-        <hr/>
         {props.children}
-        <footer>
+        <footer class="layout">
           <a href="/faq">
-            <div>FAQ</div>
+            <div id="faq">FAQ</div>
           </a>
           <h6>Developed by us</h6>
         </footer>
