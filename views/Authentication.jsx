f3957fbc42ea15b0f91bbe1be92350300d615be8
tlapi <tasha.lapitan@gmail.com>
2020-11-26 14:21:37 +0100

    style auth page, update home css

--- views/Authentication.jsx
+++ views/Authentication.jsx
@@ -4,49 +4,44 @@ const Layout = require("./Layout");
 function Authentication(props) {
   return (
     <Layout title="Login/Signup">
-      <main>
+      <main id="auth-container">
         <section>
           <h1>Sign Up</h1>
+          <hr/>
           <form action="/auth/signup" method="POST">
-            <label htmlFor="username">Username</label>
+            <label htmlFor="username">Username</label> <br/>
             <input type="text" name="username" placeholder="username"/>
             <br/>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email</label> <br/>
             <input type="email" name="email" placeholder="your@mail.com"/>
             <br/>
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password</label> <br/>
             <input type="password" name="password" placeholder="Minimum 5 characters long"/>
             <br/>
-            <label htmlFor="repeatPassword">Repeat Password</label>
+            <label htmlFor="repeatPassword">Repeat Password</label> <br/>
             <input type="password" name="repeatPassword" placeholder="Repeat your password"/>
             <br/>
-            <button type="submit">Create Account</button>
+            <button type="submit" class="click-btn">Sign Up</button>
           </form> 
-            <div>
+        </section>
+        
               { props.errorMessage
-                ? props.errorMessage
+                ? <div id="error-msg"> {props.errorMessage} </div>
                 : null
               }
-            </div>
-        </section>
-
+            
         <section>
           <h1>Log In</h1>
+          <hr/>
           <form action="/auth/login" method="POST">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email</label> <br/>
             <input type="email" name="email" placeholder="your@mail.com"/>
             <br/>
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password</label> <br/>
             <input type="password" name="password" placeholder="Minimum 5 characters long"/>
             <br/>
-            <button type="submit">Log In</button>
+            <button type="submit" class="click-btn">Log In</button>
           </form> 
-            <div>
-              { props.errorMessage
-                ? props.errorMessage
-                : null
-              }
-            </div>
         </section>
       </main>
     </Layout>
